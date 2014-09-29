e67e142fed91aece240051341029e742626b28fd
Samuel Murphy <samuel.murphy@gmail.com>
2014-09-29 17:22:10 -0400

    first cut

--- make_testdata.rb
+++ make_testdata.rb
@@ -1,5 +1,12 @@
 
-puts "test 3"
 
-puts "test stub"
+# consider making dimensions more uniform, 2.5, 5, 5   
 
+1.upto(5).each{|dim|
+
+  print dim 
+  print ", ", (rand*10).round(2)
+  print ", ", (rand*10).round(2)
+  print ", ", (rand*10).round(2)
+  puts
+}
